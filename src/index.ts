cd60204a562758eed736fd6fba8102020d64aaea
gashi-olti <oltiongashi21@gmail.com>
2024-07-25 19:03:57 +0200

    use errorHandler

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
+import 'reflect-metadata';
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { Request, Response } from 'express';
 
 import { AppDataSource } from './data-source';
 import { userRouter } from './routes/user.routes';
+import { errorHandler } from './middleware/error.middleware';
 
-import 'reflect-metadata';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(errorHandler);
 
-const { PORT } = process.env;
+const { PORT } = process.env ?? 8000;
 
 app.use('/auth', userRouter);
 
+// Catch-all route for undefined routes
 app.get('*', (req: Request, res: Response) => {
-  res.status(505).json({ message: 'Bad Request' });
+  res.status(404).json({ message: 'Not Found' });
 });
 
 AppDataSource.initialize()
